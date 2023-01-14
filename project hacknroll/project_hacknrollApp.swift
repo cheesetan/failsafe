f1a7a9d88ef04083dcb7ed35081e5e2e35739604
cheesetan <93832702+cheesetan@users.noreply.github.com>
2023-01-14 17:31:02 +0800

    Initial Commit

---
+++ project hacknroll/project_hacknrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_hacknrollApp.swift
+//  project hacknroll
+//
+//  Created by Tristan on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct project_hacknrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
